bbb2e8e314bb940d54693bd669660be341361891
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-05 10:00:45 +0300

    Added AddNote Screen + addNewNote Functionality

--- src/store/slices/generalSlice.ts
+++ src/store/slices/generalSlice.ts
@@ -1,14 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { v4 as uuid } from "uuid";
+import {
+  fetchFromLocalStorage,
+  storeInLocalStorage,
+} from "../../utils/helpers";
+import { GeneralSliceInitialState, Note } from "../../Types";
 
-const initialState = {};
+const initialState: GeneralSliceInitialState = {
+  notes: fetchFromLocalStorage("notes"),
+};
 
 export const generalSlice = createSlice({
   name: "generalSlice",
   initialState,
   reducers: {
-    setGeneralFields: (state, { payload }) => ({ ...state, ...payload }),
+    addNewNote(
+      state,
+      action: PayloadAction<{ noteTitle: string; noteContent: string }>
+    ) {
+      const { noteTitle, noteContent } = action.payload;
+      const noteId = uuid();
+      const newPost: Note = {
+        noteId,
+        noteTitle,
+        noteContent,
+        noteDate: new Date().toISOString(),
+      };
+      state.notes.push(newPost);
+      storeInLocalStorage("notes", state.notes);
+    },
   },
 });
+export const { addNewNote } = generalSlice.actions;
 
-export const { setGeneralFields } = generalSlice.actions;
 export default generalSlice.reducer;
