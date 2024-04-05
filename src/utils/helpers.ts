07c71b2b9ba4f56a6437aae2196443538d2b6426
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-05 09:57:56 +0300

    Added fetchFromLocalStorage & storeInLocalStorage Functions

---
+++ src/utils/helpers.ts
@@ -0,0 +1,15 @@
+import { Note } from "../Types";
+
+export const fetchFromLocalStorage = (itemName: string) => {
+  console.log(itemName);
+  let data = localStorage.getItem(itemName);
+  if (data) {
+    return JSON.parse(localStorage.getItem(itemName));
+  } else {
+    return [];
+  }
+};
+
+export const storeInLocalStorage = (itemName: string, data: Note) => {
+  localStorage.setItem(itemName, JSON.stringify(data));
+};
