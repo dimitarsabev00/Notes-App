c887914e954a3d4567bdb18571459af1e49cc08b
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-05 09:58:56 +0300

    Added ReactToastify.css File Import In main.tsx File

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import App from "./App.tsx";
 import "./index.scss";
 import { Provider } from "react-redux";
 import store from "./store/index.ts";
+import "react-toastify/dist/ReactToastify.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <App />
