1e0984f24341d21077c76dd48cd05123951dfa32
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-05 08:59:06 +0300

    Set Up Routing In App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
